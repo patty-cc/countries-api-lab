fd8fd53b62c92f7c3f198e41623e4209b0781b17
Nicholas Holloway <nholloway91@gmail.com>
2017-10-18 14:16:08 +0100

    can display individual country from drop down

---
+++ app.js
@@ -0,0 +1,60 @@
+var init = function () {
+  var url = "https://restcountries.eu/rest/v2/all"
+  makeRequest(url);
+}
+
+var makeRequest = function(url) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener( "load", function() {
+    var countries = JSON.parse(this.responseText);
+    addCountriesToDropDown( countries );
+  });
+  request.send();
+}
+
+var addCountriesToDropDown = function(countries) {
+  var select = document.querySelector('#country-drop-down');
+  countries.forEach(function(country, index) {
+    var option = document.createElement("option");
+    option.innerText = country.name;
+    option.value = index;
+    select.appendChild(option);
+  })
+  select.addEventListener('change', function() {
+    var div = document.querySelector('#individual-country');
+    div.innerHTML = "";
+    var nameHeading = document.createElement("h1");
+    nameHeading.innerText = countries[this.value].name;
+    var populationP = document.createElement("p");
+    populationP.innerText = "The population of " + countries[this.value].name + " is " + countries[this.value].population
+    var capitalCityP = document.createElement("p");
+    capitalCityP.innerText = "The capital of " + countries[this.value].name + " is " + countries[this.value].capital
+    div.appendChild(nameHeading);
+    div.appendChild(populationP);
+    div.appendChild(capitalCityP);
+  });
+}
+
+window.addEventListener('load', init);
+
+
+// var addCountriesToList = function(countries) {
+//   var ul = document.querySelector('#countries');
+//   for (var country of countries) {
+//     var li = document.createElement("li");
+//     li.innerText = country.name + ", " + country.population + " people live here!";
+//     ul.appendChild(li);
+//   }
+// }
+
+// var button = document.getElementById('btn');
+// button.addEventListener('click', function() {
+//   makeRequest(url);
+// })
+
+var clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', function() {
+  var div = document.getElementById("individual-country");
+  div.innerHTML = "";
+})
